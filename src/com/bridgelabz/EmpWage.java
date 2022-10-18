1e9ce6e1739a08bbb5d2a4305c355e9ef08dc5d8
saurav4986 <saurav.raj4986@gmail.com>
2022-10-18 16:25:16 +0530

    Added : UC5- calculating Employee monthly Wage

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -6,29 +6,38 @@ public class EmpWage {
     static final int EMP_WAGE_HRS = 20;
 
     public static void main(String[] args) {
-        //Use case 4 code with Switch statement
+        //Use case 5 calculating Employee monthly Wage
 
         System.out.println("Welcome to Employee wage");
 
-        int empCheck = (int)(Math.random() * 3); //Generating Random Number
         int wage = 0;
         int empHrs = 0;
+        int monthlyWage = 0;
 
-        switch (empCheck){
-            case IS_FULL_TIME :
-                System.out.println("The employee is doing full time");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is doing part time");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                empHrs = 0;
+        for(int i = 0; i<20; i++) {
+
+            int empCheck = (int)(Math.random() * 3); //Generating Random Number
+
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("The employee is doing full time");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is doing part time");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+            }
+
+            wage = empHrs * EMP_WAGE_HRS;
+            System.out.println("The employee wage is  " + wage);
+            System.out.println("---------------------------------------------------------");
+
+            monthlyWage += wage;
         }
-
-        wage = empHrs * EMP_WAGE_HRS;
-        System.out.println("The employee wage is  "+ wage);
+        System.out.println("Total monthly wage of Employee is: "+monthlyWage);
     }
 }
